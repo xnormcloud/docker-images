0dcdeca013eec4047282069d52d3b5d4c28bbd00
legendnightt <alex@legend.sh>
2022-01-30 07:50:49 +0100

    new: chatbridge: init

---
+++ chatbridge/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Prints sh info stuff
+echo Executing xnorm-chatbridge-sh
+echo Author: legendnightt
+echo Vendor: xnormnet
+echo Version: 0.0.1
+echo Website: https://xnorm.cloud
+echo Repository: https://github.com/xnormnet/docker-images
+
+#Set directory
+cd /home/container
+
+# Update python libraries
+pip3 install pycryptodome colorlog --upgrade
+
+# Print current Python3, Pip3 Version
+python3 --version
+pip3 --version
+
+# Make internal Docker IP address available to processes
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
